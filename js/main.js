c48a73966458da0a59ba66c686f77897665d2425
nais2008 <134312352+nais2008@users.noreply.github.com>
2024-04-03 17:28:35 +0500

    header

--- js/main.js
+++ js/main.js
@@ -7,3 +7,13 @@ let app = new Vue ({
     },
 });
 
+// burger menu
+const burgerButton = document.querySelector('.burger-button');
+const burgerMenu = document.querySelector('.burger-menu')
+
+function toggleBurger() {
+  burgerButton.classList.toggle('active')
+  burgerMenu.classList.toggle('active')
+}
+
+burgerButton.addEventListener('click', toggleBurger)

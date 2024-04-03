e279d2f35134e08ff7987de73d4ec1cd5ce73eac
Rayten225 <146968008+Rayten225@users.noreply.github.com>
2024-04-03 21:20:04 +0500

    никита купи чипсы

--- js/express.js
+++ js/express.js
@@ -6,11 +6,12 @@ const app = express()
 const PORT = process.env.PORT ?? 3000
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'static', '../index.html'))
+    res.sendFile(path.resolve(__dirname, '', '../index.html'))
 
 })
 
 app.listen(PORT, () => {
     console.log(`Server has been startred on port ${PORT}...`)
-}) 
+})
+
 

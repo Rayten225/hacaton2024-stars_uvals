467890cc9156d3295edbd678f31dd63b001b1ec6
nais2008 <134312352+nais2008@users.noreply.github.com>
2024-04-05 02:01:56 +0500

    check

--- include/connect.php
+++ include/connect.php
@@ -11,26 +11,3 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Соединение с базой данных не удалось: " . $conn->connect_error);
 }
-
-$login = "SELECT login FROM admin";
-$result = $conn->query($login);
-
-$password = "SELECT password FROM admin";
-$result = $conn->query($password);
-
-while ($row = $result->fetch_assoc()) {
-    echo $row['login'];
-}
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>

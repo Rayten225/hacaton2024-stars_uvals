8bf0eb251f12680c2767d80bd5de3600cb2c12a0
Rayten225 <146968008+Rayten225@users.noreply.github.com>
2024-04-05 06:08:45 +0500

    Revert "ы"
    This reverts commit 0af51a7785950e992377c1083486f22f222efca3.

---
+++ include/connect.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "stars_uvals";
+
+// Создаем соединение
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Проверяем соединение
+if ($conn->connect_error) {
+    die("Соединение с базой данных не удалось: " . $conn->connect_error);
+}
+////
+
